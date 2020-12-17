3e194f19b3c0da34f2e9fb21fe5f12011d2d3550
Clément Berges <clementberges@hotmail.fr>
2020-12-17 02:01:02 +0100

    ajout code c

---
+++ sopc_anemometre/software/test_anem_/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+  ../test_anem__bsp/system.h ../test_anem__bsp/linker.h \
+  ../test_anem__bsp//HAL/inc/sys/alt_stdio.h
+
+../test_anem__bsp/system.h:
+
+../test_anem__bsp/linker.h:
+
+../test_anem__bsp//HAL/inc/sys/alt_stdio.h:
